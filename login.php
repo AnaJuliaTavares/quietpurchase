b4a170ffe16f288f78bf520fd1270ac7624ee6ac
Ana Julia Santarém Tavares <109613769+AnaJuliaTavares@users.noreply.github.com>
2023-01-28 17:01:56 -0400

    Update login.php

--- login.php
+++ login.php
@@ -5,10 +5,16 @@ session_start();
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Logar no sistema</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/signin.css" rel="stylesheet">
 	</head>
 	<body>
-		<h2>Acessar Conta </h2>
+		<div class="container">
+			<div class="form-signin" style="background: #Add8e6;">
+			<h2 class="text-center">Acessar Conta </h2>
 		<?php
 			if(isset($_SESSION['msg'])){
 				echo $_SESSION['msg'];
@@ -20,18 +26,22 @@ session_start();
 			}
 		?>
 		<form method="POST" action="valida.php">
-			<label>Usuário</label>
+			<label>Usuário:</label>
 			<input type="text" name="usuario" placeholder="Digite o seu usuário"><br><br>
 			
-			<label>Senha</label>
+			<label>Senha:</label>
 			<input type="password" name="senha" placeholder="Digite a sua senha"><br><br>
 			
-			<input type="submit" name="btnLogin" value="Acessar">
+			<input type="submit" name="btnLogin" value="Acessar" class="btn btn-sucess btn-block">
 			
 			<h4>Você ainda não possui uma conta?</h4>
 			<a href="cadastrar.php">Criar Conta</a>
 		
 		</form>
+		</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 		<br><br>
 	</body>
 </html>
